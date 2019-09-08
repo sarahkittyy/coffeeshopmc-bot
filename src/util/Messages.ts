068fabae2183e55e4c0ade83dd754734ac2b1507
Sarah Ohlin <sarahohlin1@gmail.com>
2019-09-08 19:29:06 -0400

    renamed messages file

--- src/util/Messages.ts
+++ src/util/Messages.ts
@@ -14,10 +14,34 @@ export function MemberJoin(member: Discord.GuildMember): Discord.RichEmbed
 export function MemberLeave(member: Discord.GuildMember): Discord.RichEmbed
 {
 	return new Discord.RichEmbed()
-		.setColor(0x30ff49)
+		.setColor(0xa80505)
 		.setTitle('A user left the server ;-;')
 		.setTimestamp(new Date())
 		.setDescription(`${member.user.tag} just left-- they will be missed <3`)
 		.setFooter(`There are now ${member.guild.memberCount} people on this server <3`)
 		.setThumbnail(member.user.avatarURL);
+}
+
+export function UnknownCommand(command: string): Discord.RichEmbed
+{
+	return new Discord.RichEmbed()
+		.setColor(0xa80505)
+		.setTitle('Could not find command ' + command + '.')
+		.setTimestamp(new Date());
+}
+
+export function Error(message: string): Discord.RichEmbed
+{
+	return new Discord.RichEmbed()
+		.setColor(0xa80505)
+		.setTimestamp(new Date())
+		.setTitle(message);
+}
+
+export function Success(message: string): Discord.RichEmbed
+{
+	return new Discord.RichEmbed()
+		.setColor(0x30ff49)
+		.setTitle(message)
+		.setTimestamp(new Date());
 }
